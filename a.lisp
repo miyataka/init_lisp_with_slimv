956e0b47e6ae29f031fc02487687469d78632d7a
Takayuki Miyahara <voyager.3taka28@gmail.com>
2016-12-25 23:34:38 +0900

    Tutorial of Slimv Part2 was done.
    refference: http://kovisoft.bitbucket.org/tutorial2.html

--- a.lisp
+++ a.lisp
@@ -1,3 +1,5 @@
+;;;; Tutorial Slimv Part 1.
+
 (print "Hello World.")
 ; ,c -> swank-server start up.
 ; ,e -> eval s-expression on cursor.
@@ -12,6 +14,8 @@
   '((#\A ".-")
     (#\B "-...")
     (#\C "-.-.")
+    (#\O "---")
+    (#\S "...")
     (#\, "--..--")
     (#\? "..--..")))
 
@@ -28,3 +32,35 @@
 ; ,F -> compile whole the file.
 ; ,L -> compile and Load whole the file.
 ; <up> -> press <up> key in Insert-mode in REPL buffer evaluate latest command.
+
+;;;; Tutorial Slimv Part 2.
+
+(defun morse-to-charactor (morse-string)
+  (first (find morse-string *morse-mapping* :test #'string= :key #'second)))
+
+; ,a -> Abort in Debugger.
+; ,q -> Quit in Debugger.
+; ,n -> contiNue restart in Debugger.
+
+
+
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+; it's not work below function. why?
+(defun string-to-morse (string)
+  (with-output-to-string (morse)
+    (write-string (character-to-morse (aref string 0)) morse)
+    (loop
+      for char across (subseq string 1)
+      do (write-char #\Space morse)
+      do (write-string (character-to-morse char) morse))))
+
+; ,1 -> examine its macroexpansion.
+
+(load #P"/Users/taka/sh/quick.lisp")
+
+(quicklisp-quickstart:install)
+
+(ql:quickload :split-sequence)
+; ,i -> inspect package? or other object?
+;;;; end of Tutorial of Slimv Part 2.
